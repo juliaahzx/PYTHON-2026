9edee76220b906b91e77d2843f02bb2954c46efa
juliaahzx <jnfrederici@gmail.com>
2026-03-19 20:57:07 -0300

    aula de váriaveis e operadores

---
+++ DESAFIO 1.py
@@ -0,0 +1,8 @@
+nome = input("Digite o seu nome:")
+print (f"Seja bem vindo(a){nome}" )
+
+# desafio 2
+dia = input ("Dia do nascimento:")
+mes = input ("Dia do mes:")
+ano = input ("Ano atual")
+print(f"{dia}/{mes}/{ano}")
---
+++ aula variaveis e operadores/aula-variaveis.py
@@ -0,0 +1,35 @@
+print("oii")
+
+print(7+4)
+print("7 + 4")
+print("7" + "4") # CONCATENAÇÃO DE STRINGS
+
+# variáveis
+nome = "júlia" #str
+print(nome)
+idade = 18 # int
+peso = 51 # float
+print(nome ,idade , peso)
+
+print(f"oiiii, {nome}!!!")
+
+#INPUT - SIMULAÇÃO DE FORMS NO CMD
+nome = input("Digite o seu nome:")
+idade = input("Digite sua idade")
+
+print(nome,idade)
+
+ano_nascimento = 2007
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+
+#
+
+
+
+
+
+
+
+
+
